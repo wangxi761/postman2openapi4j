edf384fe948c9dbf7902c57970c5e2e41d198a1e
wangxi <wangxi3@huya.com>
2021-09-06 23:18:06 +0800

    add postman collection schema

---
+++ src/main/java/org/xi/postman2openapi4j/eneity/v21/PostmanCollectionV21.java
@@ -0,0 +1,29 @@
+package org.xi.postman2openapi4j.eneity.v21;
+
+public class PostmanCollectionV21 {
+	
+	
+	public static class Information {
+	
+	}
+	
+	public static class Items {
+	
+	}
+	
+	public static class EventList {
+	
+	}
+	
+	public static class VariableList {
+	
+	}
+	
+	public static class Auth {
+	
+	}
+	
+	public static class ProtocolProfileBehavior {
+	
+	}
+}
